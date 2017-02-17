6945064b09cc1507c7ddceb7a31d86fd2e311458
Elvis Rozario <elvisrozarioefr@gmail.com>
2017-02-17 16:49:32 +0530

    Added dummy products json file

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Shops
+from .models import Shops, Products
 
 admin.site.register(Shops)
+admin.site.register(Products)
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,11 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Products
 
 def index(request):
     all_products = Products.objects.all()
-    template = loader.get_template('shop/index.html')
-    context = {
-        'all_products' : all_products,
-    }
-    # html = ''
-    # for product in all_products:
-    #     url = 'shop/'+str(product.id)+'/'
-    #     html+='<a href="'+url+'">'+product.name+'</a><br>'
-    return HttpResponse(template.render(context, request))
+    context = {'all_products' : all_products}
+    return render(request, 'shop/index.html', context)
 
 def detail(request, product_id):
     return HttpResponse('<h2>Details for Product ID :'+str(product_id)+' </h2>')
