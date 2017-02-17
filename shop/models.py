da5bef7ba5e7c27c55cf4c11171f35fdd47a56bf
Elvis Rozario <elvisrozarioefr@gmail.com>
2017-02-17 12:26:07 +0530

    Added shops and products models
    Added fistures for shops as well

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Shops(models.Model):
+    name = models.CharField(max_length=200)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+    category = models.CharField(max_length=200)
+    image = models.CharField(max_length=1000)
+    shops = models.ManyToManyField(Shops, through='ShopsProducts')
+
+    def __str__(self):
+        return self.name
+
+class ShopsProducts(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shops, on_delete=models.CASCADE)
