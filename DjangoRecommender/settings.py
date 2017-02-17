0f3b5fa78036e77788620992552692715026e4ab
( ͡° ͜ʖ ͡°) <isaac.v.nadar@gmail.com>
2017-02-17 11:24:32 +0530

    gmaps settings

--- DjangoRecommender/settings.py
+++ DjangoRecommender/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+GOOGLE_API_KEY = 'AIzaSyDT4hsIgh9REOzF-HRpi3Qz41rwMFreSdo'
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gmaps',
 ]
 
 MIDDLEWARE = [
