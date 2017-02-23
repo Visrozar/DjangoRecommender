3bc518ec9a4640cac2ee2fb40a531a3d135f916b
Visrozar <elvisrozarioefr@gmail.com>
2017-02-23 13:12:15 +0530

    Individual Product page reached

--- DjangoRecommender/urls.py
+++ DjangoRecommender/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^shop/', include('shop.urls')),
+    url(r'^shop', include('shop.urls')),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,36 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Products
 
 def index(request):
     all_products = Products.objects.all()
-    kitchen_products = Products.objects.filter(category = 'kitchen')
     context = {'all_products' : all_products}
     return render(request, 'shop/index.html', context)
 
+def kitchen(request):
+    products = Products.objects.filter(category = 'kitchen')
+    context = {'all_products' : products}
+    return render(request, 'shop/index.html', context)
+
+def sports(request):
+    products = Products.objects.filter(category = 'sports')
+    context = {'all_products' : products}
+    return render(request, 'shop/index.html', context)
+
+def electronics(request):
+    products = Products.objects.filter(category = 'electronics')
+    context = {'all_products' : products}
+    return render(request, 'shop/index.html', context)
+
+def clothing(request):
+    products = Products.objects.filter(category = 'clothing')
+    context = {'all_products' : products}
+    return render(request, 'shop/index.html', context)
+
 def detail(request, product_id):
-    product = Products.objects.get(pk=product_id)
+    try:
+        product = Products.objects.get(pk=product_id)
+    except Exception:
+        raise Http404("No product !!!")
     context = {'product' : product}
     return render(request, 'shop/product.html', context)
