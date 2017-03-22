5860f54ae572ab545ac4a62e48fa58f05f107d0c
Visrozar <elvisrozarioefr@gmail.com>
2017-03-22 14:50:15 +0530

    New Changes

--- shop/views.py
+++ shop/views.py
@@ -12,7 +12,8 @@ def detail(request, product_id):
     try:
         # reco = Products.objects.filter(item=product_id)
         reco = Recommends.objects.filter(item=product_id)
+        movie = Products.objects.all()[:1].get()
     except Exception:
         raise Http404("No product !!!")
-    context = {'product' : reco}
+    context = {'product' : reco, 'current' : movie}
     return render(request, 'shop/product.html', context)
